4cfda527d31e9ea87e37db4984f878f6e224d48b
ablueway <ablueway@gmail.com>
2018-01-13 00:50:13 +0800

    fix build error cause the code svn rebase issue

---
+++ host/os_wrapper/linux/os.c
@@ -0,0 +1,256 @@
+/*
+*  (C) Copyright 2014-2016 Shenzhen South Silicon Valley microelectronics co.,limited
+*
+*  All Rights Reserved
+*/
+#include <linux/kernel.h>
+#include <linux/random.h>
+#include <linux/spinlock.h>
+#include <linux/semaphore.h>
+//#include <semaphore.h>
+#include <linux/mutex.h>
+#include <linux/delay.h>
+#include <linux/slab.h>
+#include <linux/kthread.h>
+#include <linux/timer.h>
+
+#include "os.h"
+/*TOSO: aaron */
+//#include "os_cfg.h"
+//#include <log.h>
+
+
+
+volatile u8 gOsFromISR;
+//static u8 os_init_flag = 0;
+
+#define STATIC_STK
+#ifdef STATIC_STK
+//(TOTAL_STACK_SIZE<<4) --> transfer unit to bytes
+//((TOTAL_STACK_SIZE<<4)>>2) --> transfer unit to word
+u32 task_stack[((TOTAL_STACK_SIZE<<4)>>2)];
+u32 stk_used=0;
+OsMutex StkMutex;
+#endif
+
+u32 task_stack_size = 0;
+
+spinlock_t g_os_cs_lock;
+unsigned long g_os_cpu_flags;
+
+wait_queue_head_t g_os_task_wait_q;
+
+OS_APIs s32 OS_Init( void )
+{
+	spin_lock_init(&g_os_cs_lock);
+	init_waitqueue_head(&g_os_task_wait_q);
+    return OS_SUCCESS;
+}
+
+OS_APIs unsigned long OS_Random(void)
+{
+	unsigned long randNum = 0;
+	get_random_bytes(&randNum, sizeof(unsigned long));
+    return randNum;
+}
+
+OS_APIs void OS_Terminate(void)
+{
+
+}
+
+OS_APIs u32 OS_EnterCritical(void)
+{
+	spin_lock_irqsave(&g_os_cs_lock, g_os_cpu_flags);
+    return OS_SUCCESS;
+}
+
+OS_APIs void OS_ExitCritical(u32 val)
+{
+	spin_unlock_irqrestore(&g_os_cs_lock, g_os_cpu_flags);
+}
+
+/* Task: */
+OS_APIs s32 OS_TaskCreate(OsTask task, const char *name, u32 stackSize, 
+					void *param, u32 pri, OsTaskHandle *taskHandle)
+{
+	/* TODO: aaron 
+	 * For now, task create the sys will hit two kind of kernel panic :
+	 * 1. the null pointer of task function (ex: tx/rx)
+     * 	  the task function need to review and re-program. temply mark it.
+     * 2. for workaround step 1, the task will block too long to make kernel 
+     *    report this as issue. temply mark the api content.				
+	 */
+#if 0
+	struct task_struct *new_task = kthread_create(task, param, name);
+	if (IS_ERR(new_task))
+	{
+		return OS_FAILED;
+	}
+	wake_up_process(new_task);
+#endif
+	return OS_SUCCESS;
+}
+
+
+OS_APIs void OS_TaskDelete(OsTaskHandle taskHandle)
+{
+	kthread_stop((struct task_struct *)(taskHandle));
+}
+
+
+OS_APIs void OS_StartScheduler(void)
+{
+
+}
+
+OS_APIs u32 OS_GetSysTick(void)
+{
+    return OS_SUCCESS;
+}
+
+
+/* Mutex APIs: */
+OS_APIs s32 OS_MutexInit(OsMutex *mutex)
+{
+//	struct mutex *lock = (struct mutex *)mutex;
+	*mutex = kzalloc(sizeof(struct mutex), GFP_KERNEL);
+	mutex_init(*mutex);
+    return OS_SUCCESS;
+}
+
+OS_APIs void OS_MutexLock(OsMutex mutex)
+{
+	mutex_lock(mutex);
+}
+
+OS_APIs void OS_MutexUnLock(OsMutex mutex)
+{
+	mutex_unlock(mutex);
+}
+
+OS_APIs void OS_MutexDelete(OsMutex mutex)
+{
+
+}
+
+OS_APIs void OS_TickDelay(u32 ticks)
+{
+
+}
+
+
+OS_APIs void OS_MsDelay(u32 ms)
+{
+	msleep(ms);
+}
+
+#define SEMA_LOCK()		OSSchedLock()
+#define SEMA_UNLOCK()		OSSchedUnlock()
+
+OS_APIs s32 OS_SemInit(OsSemaphore *Sem, u16 maxcnt, u16 cnt)
+{
+//	struct semaphore *sem = (struct semaphore *)Sem;
+	*Sem = kzalloc(sizeof (struct semaphore), GFP_KERNEL);
+	sema_init(*Sem, maxcnt);
+	return OS_SUCCESS;
+}
+
+OS_APIs bool OS_SemWait(OsSemaphore Sem , u16 timeout)
+{
+//	struct semaphore *sem = (struct semaphore *)Sem;
+//	down(sem);
+	return down_timeout(Sem, timeout);
+//    return OS_SUCCESS;
+}
+
+OS_APIs u8 OS_SemSignal(OsSemaphore Sem)
+{
+	//struct semaphore *sem = (struct semaphore *)Sem;
+
+	up(Sem);
+
+    return OS_SUCCESS;
+}
+
+OS_APIs u32 OS_SemCntQuery(OsSemaphore Sem)
+{
+    return OS_SUCCESS;
+}
+
+OS_APIs u8 OS_SemSignal_FromISR(OsSemaphore Sem)
+{
+    return OS_SUCCESS;
+}
+
+OS_APIs void OS_SemDelete(OsSemaphore Sem)
+{
+//	struct semaphore *sem = (struct semaphore *)Sem;
+//	sem_destroy(sem);
+	kfree(Sem);
+}
+
+#define MSGQ_LOCK()		OSSchedLock()
+#define MSGQ_UNLOCK()		OSSchedUnlock()
+
+/* Message Queue: */
+OS_APIs s32 OS_MsgQCreate( OsMsgQ *MsgQ, u32 QLen )
+{
+    return OS_SUCCESS;
+}
+
+OS_APIs s32 OS_MsgQDelete( OsMsgQ MsgQ)
+{
+    return OS_SUCCESS;
+}
+
+OS_APIs s32 OS_MsgQEnqueue( OsMsgQ MsgQ, OsMsgQEntry *MsgItem, bool fromISR )
+{
+    return OS_SUCCESS;
+}
+
+OS_APIs s32 OS_MsgQEnqueueTry( OsMsgQ MsgQ, OsMsgQEntry *MsgItem, bool fromISR )
+{
+    return OS_SUCCESS;
+}
+
+OS_APIs s32 OS_MsgQDequeue( OsMsgQ MsgQ, OsMsgQEntry *MsgItem, u32 timeOut, bool fromISR )
+
+{
+    return OS_SUCCESS;
+}
+
+
+OS_APIs s32 OS_MsgQWaitingSize( OsMsgQ MsgQ )
+{
+    return OS_SUCCESS;
+}
+
+
+
+/* Timer: */
+OS_APIs s32 OS_TimerCreate(OsTimer *timer, u32 ms, u8 autoReload, void *args, OsTimerHandler timHandler)
+{    
+	return OS_SUCCESS;
+}
+
+OS_APIs s32 OS_TimerSet(OsTimer timer, u32 ms, u8 autoReload, void *args)
+{
+    return OS_SUCCESS;
+}
+
+OS_APIs s32 OS_TimerStart(OsTimer timer)
+{
+    return OS_SUCCESS;
+}
+
+OS_APIs s32 OS_TimerStop(OsTimer timer)
+{
+    return OS_SUCCESS;
+}
+
+OS_APIs void *OS_TimerGetData(OsTimer timer)
+{
+   return NULL;
+}
+
---
+++ host/platform/linux/porting.c
@@ -0,0 +1,109 @@
+/*
+*  (C) Copyright 2014-2016 Shenzhen South Silicon Valley microelectronics co.,limited
+*
+*  All Rights Reserved
+*/
+#include <linux/slab.h>
+#include <linux/delay.h>
+#include <linux/string.h>
+
+#define _PORTING_C_
+#include <os.h>
+#include <porting.h>
+#include <ssv_lib.h>
+
+
+
+
+
+#define APP_INCLUDE_WIFI_FW
+
+/*=============console==================*/
+u8 hal_getchar(void)
+{
+        return 0;
+}
+
+/*=============Memory==================*/
+OS_APIs void *OS_MemAlloc(u32 size)
+{
+	/**
+	 *  Platform dependent code. Please rewrite
+	 *  this piece of code for different system.
+	 */
+    void *ptr = NULL;
+    ptr = kzalloc(size, GFP_KERNEL);
+    return ptr;
+}
+
+OS_APIs void __OS_MemFree(void *m)
+{
+    /**
+	 *  Platform depedent code. Please rewrite
+	 *  this piece of code for different system.
+	 */
+    kfree(m);
+}
+
+
+void OS_MemCPY(void *pdest, const void *psrc, u32 size)
+{
+	memcpy(pdest, psrc, size);
+}
+
+void OS_MemSET(void *pdest, u8 byte, u32 size)
+{
+    memset(pdest, byte, size);
+}
+
+
+/* TODO: aaron 
+ * Should Linux need this or just GP platform need ??
+ */
+//=====================Platform LDO EN ping setting=======================
+void platform_ldo_en_pin_init(void)
+{
+#ifdef __linux__
+
+#else
+/* in */
+#ifdef SSV_LDO_EN_PIN
+/* not in */
+#if (HOST_PLATFORM_SEL == GP_19B)
+    gpio_set_memcs(1,0); //cs1 set as IO
+#endif
+    gpio_init_io(SSV_LDO_EN_PIN,GPIO_OUTPUT);
+    gpio_set_port_attribute(SSV_LDO_EN_PIN, 1);
+    gpio_write_io(SSV_LDO_EN_PIN, 0);
+#endif
+#endif
+}
+
+void platform_ldo_en(bool en)
+{
+#ifdef __linux__
+
+#else
+/* in */
+#ifdef SSV_LDO_EN_PIN
+    gpio_write_io(SSV_LDO_EN_PIN, en);
+#endif
+#endif
+}
+
+//=====================find fw to download=======================
+#include <ssv_hal.h>
+bool platform_download_firmware(void)
+{
+    return ssv6xxx_download_fw((u8 *)SSV_HAL_FW_BIN, sizeof(SSV_HAL_FW_BIN));
+}
+void platform_read_firmware(void *d,void *s,u32 len)
+{
+    OS_MemCPY(d,s,len);
+}
+
+void platform_udelay(u32 us_delay)
+{
+    udelay(us_delay/1000);
+}   
+
